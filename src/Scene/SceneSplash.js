364e77672b84d84d689db5a35166deaa33d72ffa
stdmatt <stdmatt@pixelwizards.io>
2019-10-08 14:28:40 +0300

    Set the text positions.

--- src/Scene/SceneSplash.js
+++ src/Scene/SceneSplash.js
@@ -1,4 +1,4 @@
-const SPLASH_SCENE_FONT_SIZE = 35;
+const SPLASH_SCENE_FONT_SIZE = 40;
 
 class SceneSplash
     extends Base_Scene
@@ -8,8 +8,23 @@ class SceneSplash
     {
         super();
 
+        //
+        // iVars.
+        // Properties.
         this.stdmattText  = new Text("stdmatt",  SPLASH_SCENE_FONT_SIZE);
         this.presentsText = new Text("presents", SPLASH_SCENE_FONT_SIZE);
+
+        //
+        // Initialize.
+        const screen_size = Get_Screen_Size();
+
+        this.stdmattText.pivot.set(0.5);
+        this.stdmattText.x = (screen_size.x * 0.5);
+        this.stdmattText.y = (screen_size.y * 0.5) - this.stdmattText.height;
+
+        this.presentsText.pivot.set(0.5);
+        this.presentsText.x = (screen_size.x * 0.5);
+        this.presentsText.y = (screen_size.y * 0.5) + this.presentsText.height;
 
         this.addChild(this.stdmattText );
         this.addChild(this.presentsText);
