578c00e0feceed915fa46601790e583eb1823257
stdmatt <stdmatt@pixelwizards.io>
2019-10-15 17:33:40 +0300

    Make the block blinks before get squashed.

--- src/GamePlay/Block.js
+++ src/GamePlay/Block.js
@@ -8,8 +8,14 @@
 //------------------------------------------------------------------------------
 const BLOCK_COLOR_INDEX_COUNT = 2;
 const BLOCK_BORDER_SIZE       = 1;
-const BLOCK_DESTROY_TWEEN_DURATION_MS = 3500;
+
+const BLOCK_DESTROY_TWEEN_DURATION_MS = 1500;
 const BLOCK_DESTROY_TWEEN_EASING      = TWEEN.Easing.Circular.In
+
+const BLOCK_BLINK_TWEEN_DURATION_MS       = 300;
+const BLOCK_BLINK_TWEEN_BLINK_COUNT       = 4;
+const BLOCK_BLINK_TWEEN_BLINK_DURATION_MS = (BLOCK_BLINK_TWEEN_DURATION_MS / BLOCK_BLINK_TWEEN_BLINK_COUNT);
+
 
 //------------------------------------------------------------------------------
 let S_BLOCK_OBJECT_ID = 0;
@@ -43,16 +49,19 @@ class Block
         this.isDestroying = false;
         this.destroyValue = 0;
 
-        // Drawing.
-        this.graphics = new PIXI.Graphics();
-        this._DrawGraphics();
-        this.addChild(this.graphics);
+        const size = Vector_Copy(this.boardRef.blockSize);
+        size.x -= 0;
+        size.y -= 1;
+
+        this.sprite = Sprite_White(size);
+        Apply_BlockTintEffect(this.sprite, gPalette.GetBlockColor(this.colorIndex));
+        this.addChild(this.sprite);
 
         // Debug.
-        let text = new PIXI.Text(this.objectId,{fontFamily : 'Arial', fontSize: 24, fill : 0xFFFFFF, align : 'left'});
-        text.x = this.width  / 2 - text.width  / 2;
-        text.y = this.height / 2 - text.height / 2;
-        this.addChild(text);
+        // let text = new PIXI.Text(this.objectId,{fontFamily : 'Arial', fontSize: 24, fill : 0xFFFFFF, align : 'left'});
+        // text.x = this.width  / 2 - text.width  / 2;
+        // text.y = this.height / 2 - text.height / 2;
+        // this.addChild(text);
     } // ctor
 
     //--------------------------------------------------------------------------
@@ -67,45 +76,54 @@ class Block
         this.isDestroying = true;
         this.destroyValue = 0;
 
-        let curr = {value: 0};
-        let end  = {value: 1};
-
-        let tween = new TWEEN.Tween(curr, this.boardRef.destroyTweenGroup)
-            .to(end, BLOCK_DESTROY_TWEEN_DURATION_MS)
-            .onUpdate(()=>{
-                this.destroyValue = curr.value;
-                this._DrawGraphics();
-            })
-            .onComplete(()=>{
-                this.graphics.destroy();
-                this.boardRef.RemoveBlock(this);
-            })
-            .easing(BLOCK_DESTROY_TWEEN_EASING)
-            .start();
+        const blink  = this._CreateBlinkAnimation();
+        blink.start();
     } // StartDestroyAnimation
 
     //--------------------------------------------------------------------------
-    _DrawGraphics()
+    _CreateSquashAnimation()
     {
-        const size  = this.boardRef.blockSize;
-        const color = gPalette.GetBlockColor(this.colorIndex);
+        let tween = Tween_CreateBasic(
+                BLOCK_DESTROY_TWEEN_DURATION_MS,
+                this.boardRef.destroyTweenGroup
+            )
+            .onComplete(()=>{
+                this.boardRef.RemoveBlock(this);
+            })
+            .easing(BLOCK_DESTROY_TWEEN_EASING)
 
-        this.graphics.clear();
-        const x = BLOCK_BORDER_SIZE;
-        const y = size.y * (this.destroyValue / 2) + (BLOCK_BORDER_SIZE / 2);
-        const w = size.x - BLOCK_BORDER_SIZE;
-        const h = size.y - (y * 2) - BLOCK_BORDER_SIZE;
+        return tween;
+    } // _CreateSquashAnimation
 
+    //--------------------------------------------------------------------------
+    _CreateBlinkAnimation()
+    {
+        // @notice(stdmatt): Just to make easier to think how much the block
+        // should blink. So when we think that it needs to blink 2 times,
+        // we need to actually blink 4, because the tween actually "counts"
+        // each transition as a blink.
+        const repeat_ms = (BLOCK_BLINK_TWEEN_BLINK_COUNT  % 2 == 0)
+            ? BLOCK_BLINK_TWEEN_BLINK_COUNT + 2
+            : BLOCK_BLINK_TWEEN_BLINK_COUNT + 1;
 
-        // @XXX(stdmatt): chroma get a function to get the packed rgb value.
-        let a = chroma(color).darken(1).hex().substr(1);
-        let b = parseInt(a, 16);
+        let tween = Tween_Create()
+            .from({t:0})
+            .to({t:1}, BLOCK_BLINK_TWEEN_BLINK_DURATION_MS)
+            .repeat(repeat_ms)
+            .yoyo(true)
+            .onRepeat(()=>{
+                const color = (!tween._reversed)
+                    ? gPalette.GetBlockColor     (this.colorIndex)
+                    : gPalette.GetBlockBlinkColor(this.colorIndex);
 
-        // debugger;
-        this.graphics.lineStyle(BLOCK_BORDER_SIZE * 2, b, 1);
-        this.graphics.beginFill(color, 1);
-            // this.graphics.drawRoundedRect(x, y, w, h, 4 * (1 - this.destroyValue));
-            this.graphics.drawRect(x, y, w, h);
-        this.graphics.endFill();
-    }
+                this.sprite.blockTintEffect.SetColor(color);
+            })
+            .onComplete(()=>{
+                const squash = this._CreateSquashAnimation();
+                Apply_BlockSquashEffect(this.sprite, squash);
+                squash.start();
+            });
+
+        return tween;
+    } // _CreateBlinkAnimation
 }; // class Block
