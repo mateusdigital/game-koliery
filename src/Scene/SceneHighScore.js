650b1d5f81215890bf31b34ff12893fc9ec8ab2a
stdmatt <stdmatt@pixelwizards.io>
2019-10-16 18:48:00 +0300

    Refactor the creation of to functions.

--- src/Scene/SceneHighScore.js
+++ src/Scene/SceneHighScore.js
@@ -6,12 +6,16 @@ const HIGHSCORE_SCENE_FONT_SIZE                     = 40;
 const HIGHSCORE_SCENE_TEXT_EFFECT_DURATION_MS       = 300;
 const HIGHSCORE_SCENE_TEXT_EFFECT_DELAY_DURATION_MS = 300;
 
+const HISCORE_SCENE_OPTIONS_NONE                  = 0;
+const HISCORE_SCENE_OPTIONS_GO_BACK_AUTOMATICALLY = 1;
+const HISCORE_SCENE_OPTIONS_EDITABLE              = 2;
+
 //------------------------------------------------------------------------------
 class SceneHighScore
     extends Base_Scene
 {
     //--------------------------------------------------------------------------
-    constructor(sceneToGoBackClass, goBackAutomatically)
+    constructor(sceneToGoBackClass, options)
     {
         super();
 
@@ -30,21 +34,47 @@ class SceneHighScore
             {name:"mm9", score:"99999"},
             {name:"m10", score:"99999"},
         ];
+
         this.titleText          = null;
         this.titleLine          = null;
         this.scoreTexts         = [];
         this.scoreTweenGroup    = Tween_CreateGroup();
         this.sceneToGoBackClass = sceneToGoBackClass;
+        this.options            = options;
 
         //
         // Initialize.
+        this._CreateTitleUI();
+        this._CreateScoreUI();
+    } // ctor
+
+
+    //--------------------------------------------------------------------------
+    Update(dt)
+    {
+        this.scoreTweenGroup.update();
+        // Viewing high cores.
+        if(this.options != HISCORE_SCENE_OPTIONS_EDITABLE) {
+            if(IsKeyPress(KEY_SPACE) || IsKeyPress(KEY_ENTER)) {
+                Go_To_Scene(this.sceneToGoBackClass);
+            }
+        }
+        // Adding new high score.
+        else if(this.options == HISCORE_SCENE_OPTIONS_EDITABLE) {
+
+        }
+    } // Update
+
+    //--------------------------------------------------------------------------
+    _CreateTitleUI()
+    {
         const screen_size = Get_Screen_Size();
 
         // Title Text.
         this.titleText = Create_Normal_Text("HIGH SCORES", 50);
         Apply_TextGradientEffect(this.titleText, gPalette.GetScoreColor(0));
 
-        this.titleText.x = (screen_size.x * 0.5);
+        this.titleText.x = (screen_size.x         * 0.5);
         this.titleText.y = (this.titleText.height * 0.5) + 50;
         this.addChild(this.titleText);
 
@@ -54,36 +84,12 @@ class SceneHighScore
 
         this.titleLine.width  = this.titleText.width;
         this.titleLine.height = 15;
+
         this.titleLine.x = this.titleText.x - (this.titleText.width  * 0.5);
         this.titleLine.y = this.titleText.y + (this.titleText.height * 0.5);
 
         this.addChild(this.titleLine);
-
-        // Scores.
-        this._CreateScoreUI();
-
-        // Tween Completion.
-        this.scoreTweenGroup.onComplete(()=>{
-            /// @XXX
-            if(goBackAutomatically) {
-                setTimeout(()=>{
-                    Go_To_Scene(this.sceneToGoBackClass);
-                }, 500)
-            }
-        });
-    } // ctor
-
-
-    //--------------------------------------------------------------------------
-    Update(dt)
-    {
-        if(IsKeyPress(KEY_SPACE) || IsKeyPress(KEY_ENTER)) {
-            Go_To_Scene(this.sceneToGoBackClass);
-        }
-
-        this.scoreTweenGroup.update();
-    } // Update
-
+    } // _CreateTitleUI
 
     //--------------------------------------------------------------------------
     _CreateScoreUI()
@@ -92,14 +98,19 @@ class SceneHighScore
         const initial_y   = this.titleLine.y + this.titleLine.height + 20;
 
         for(let i = 0; i < this.scoresInfo.length; ++i) {
+            // Tween.
+            const tween = Tween_CreateBasic(
+                HIGHSCORE_SCENE_TEXT_EFFECT_DURATION_MS,
+                this.scoreTweenGroup
+            )
+            .delay(HIGHSCORE_SCENE_TEXT_EFFECT_DELAY_DURATION_MS * (i + 1))
+            .start();
+
+            // Text.
             const info = this.scoresInfo[i];
-            const str  = this._BuildScoreString(i + 1, info);
-            const text = Create_Normal_Text(str, 40);
-
-            const tween = this._CreateEffectTween(i + 1);
-            tween.start();
-
+            const str   = this._BuildScoreString(i + 1, info);
             const color = chroma(gPalette.GetScoreColor(i));
+            const text = Create_Normal_Text(str, 40);
             Apply_TextUncoverEffect (text, tween);
             Apply_TextGradientEffect(text, color);
 
@@ -108,6 +119,16 @@ class SceneHighScore
 
             this.addChild(text);
             this.scoreTexts.push(text);
+        }
+
+        // Animation Finished.
+        if(this.options == HISCORE_SCENE_OPTIONS_GO_BACK_AUTOMATICALLY) {
+            this.scoreTweenGroup.onComplete(()=>{
+                 /// @XXX
+                setTimeout(()=>{
+                    Go_To_Scene(this.sceneToGoBackClass);
+                }, 500)
+            });
         }
     } // _CreateScoreUI
 
@@ -120,16 +141,4 @@ class SceneHighScore
 
         return String_Cat(pos_str, ".", " ", name_str, " ", score_str);
     } // _BuildScoreString
-
-    //--------------------------------------------------------------------------
-    _CreateEffectTween(index)
-    {
-        const tween = Tween_CreateBasic(
-                HIGHSCORE_SCENE_TEXT_EFFECT_DURATION_MS,
-                this.scoreTweenGroup
-            )
-            .delay(HIGHSCORE_SCENE_TEXT_EFFECT_DELAY_DURATION_MS * index);
-
-        return tween;
-    } // _CreateEffectTween
 }; // class SceneHighScore
